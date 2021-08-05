65cebc1d33a54cb17670f4b8812f06e70619e84a
dutchman <dutchman@dutchmans-MacBook-Pro.local>
2021-08-05 18:37:26 +0600

    fetch data from api

---
+++ SwiftUI-MVVM/Utitlities/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SwiftUI-MVVM
+//
+//  Created by dutchman on 5/8/21.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case unableToComplete
+    case invalidResponse
+    case invalidData
+}
---
+++ SwiftUI-MVVM/Views/LoadingView.swift
@@ -0,0 +1,25 @@
+//
+//  LoadingView.swift
+//  SwiftUI-MVVM
+//
+//  Created by dutchman on 5/8/21.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground).edgesIgnoringSafeArea(.all)
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .gray))
+                .scaleEffect(2)
+        }
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
