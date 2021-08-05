2a1ba574eb8b7287c2bf8c689385c3dfbb6693bf
dutchman <jewelms007@gmail.com>
2021-08-06 00:52:35 +0600

    viewModel added

--- SwiftUI-MVVM/Views/AppetizersListView/AppetizerListView.swift
+++ SwiftUI-MVVM/Views/AppetizersListView/AppetizerListView.swift
@@ -9,47 +9,21 @@ import SwiftUI
 
 struct AppetizerListView: View {
     
-    @State private var appetizers: [Appetizer] = []
-    @State private var isLoading = false
-    @State private var alertItem: AlertItem?
+    @StateObject private var viewModel = AppetizerListViewModel()
     
     var body: some View {
         ZStack {
             NavigationView {
-                List(appetizers, id: \.id){ appetizer in
+                List(viewModel.appetizers, id: \.id){ appetizer in
                     AppetizerCell(appetizer: appetizer)
                 }.navigationTitle(" Appetizers")
             }.onAppear {
-                getAppetizers()
+                viewModel.getAppetizers()
             }
-            if isLoading { LoadingView() }
+            if viewModel.isLoading { LoadingView() }
         }
-        .alert(item: $alertItem){ alertItem in
+        .alert(item: $viewModel.alertItem){ alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
-        }
-    }
-    
-    func getAppetizers(){
-        isLoading = true
-        NetworkManager.shared.getAppetizers { [self] result in
-            DispatchQueue.main.async {
-                isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    switch error {
-                    case .invalidData:
-                        alertItem = AlertContext.invalidData
-                    case .invalidURL:
-                        alertItem = AlertContext.invalidURL
-                    case .invalidResponse:
-                        alertItem = AlertContext.invalidResponse
-                    case .unableToComplete:
-                        alertItem = AlertContext.unableToComplete
-                    }
-                }
-            }
         }
     }
     
